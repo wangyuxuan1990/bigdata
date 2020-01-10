bd3d6a47712038afbea43028710788fd5117c906
王宇轩 <165140760@qq.com>
2020-01-10 16:51:33 +0800

    day06_flume

---
+++ day06_flume/src/main/java/com/wangyuxuan/flume/demo2/MySqlSource.java
@@ -0,0 +1,105 @@
+package com.wangyuxuan.flume.demo2;
+
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.PollableSource;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.event.SimpleEvent;
+import org.apache.flume.source.AbstractSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author wangyuxuan
+ * @date 2020/1/10 13:47
+ * @description flume自定义Source
+ */
+public class MySqlSource extends AbstractSource implements Configurable, PollableSource {
+    /**
+     * 打印日志
+     */
+    private final static Logger LOG = LoggerFactory.getLogger(MySqlSource.class);
+    /**
+     * 定义sqlHelper
+     */
+    private QueryMysql sqlSourceHelper;
+
+    /**
+     * 接受mysql表中的数据
+     *
+     * @return
+     * @throws EventDeliveryException
+     */
+    @Override
+    public Status process() throws EventDeliveryException {
+        try {
+            // 查询数据表
+            List<List<Object>> result = sqlSourceHelper.executeQuery();
+            // 存放event的集合
+            List<Event> events = new ArrayList<>();
+            // 存放event头集合
+            Map<String, String> header = new HashMap<>();
+            // 如果有返回数据，则将数据封装为event
+            if (!result.isEmpty()) {
+                List<String> allRows = sqlSourceHelper.getAllRows(result);
+                Event event = null;
+                for (String row : allRows) {
+                    event = new SimpleEvent();
+                    event.setBody(row.getBytes());
+                    event.setHeaders(header);
+                    events.add(event);
+                }
+                // 将event写入channel
+                this.getChannelProcessor().processEventBatch(events);
+                // 更新数据表中的offset信息
+                sqlSourceHelper.updateOffset2DB(result.size());
+            }
+            // 等待时长
+            Thread.sleep(sqlSourceHelper.getRunQueryDelay());
+            return Status.READY;
+        } catch (InterruptedException e) {
+            LOG.error("Error procesing row", e);
+            return Status.BACKOFF;
+        }
+
+    }
+
+    @Override
+    public long getBackOffSleepIncrement() {
+        return 0;
+    }
+
+    @Override
+    public long getMaxBackOffSleepInterval() {
+        return 0;
+    }
+
+    @Override
+    public void configure(Context context) {
+        // 初始化
+        try {
+            sqlSourceHelper = new QueryMysql(context);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public synchronized void stop() {
+        LOG.info("Stopping sql source {} ...", getName());
+        try {
+            // 关闭资源
+            sqlSourceHelper.close();
+        } finally {
+            super.stop();
+        }
+
+    }
+}
